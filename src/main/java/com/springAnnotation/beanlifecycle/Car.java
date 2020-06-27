f81976dd8644633925a10b5e35068fa243787219
510988896@qq.com <FANbin8807915>
2020-06-27 12:13:33 +0800

    生命周期

---
+++ src/main/java/com/springAnnotation/beanlifecycle/Car.java
@@ -0,0 +1,1 @@
+package com.springAnnotation.beanlifecycle;public class Car {    public Car() {        System.out.println("car construct method");    }    public void init(){        System.out.println("car init");    }    public void destroy(){        System.out.println("car destroy");    }}
