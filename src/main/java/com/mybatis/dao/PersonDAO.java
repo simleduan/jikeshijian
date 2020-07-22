4bc8769aea342a2f512748911d1a34fd26c15b54
510988896@qq.com <FANbin8807915>
2020-07-23 05:49:13 +0800

    jdbc

---
+++ src/main/java/com/mybatis/dao/PersonDAO.java
@@ -0,0 +1,17 @@
+package com.mybatis.dao;
+
+import com.mybatis.pojo.Person;
+
+public interface PersonDAO {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Person record);
+
+    int insertSelective(Person record);
+
+    Person selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Person record);
+
+    int updateByPrimaryKey(Person record);
+}
